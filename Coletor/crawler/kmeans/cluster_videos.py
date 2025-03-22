8aa9fc55bf9a3b48268be7b997055968527cec65
lucas <lucascarneiromalta@outlook.com>
2025-03-22 00:17:18 -0300

    Algoritmo de clusterização dos vídeos coletados

---
+++ Coletor/crawler/kmeans/cluster_videos.py
@@ -0,0 +1,282 @@
+from typing import List, Tuple, Dict
+import numpy as np
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import MinMaxScaler
+
+import matplotlib.pyplot as plt
+
+'''
+    Função para plotar o gráfico do Elbow Method
+    @param features - DataFrame com os dados a serem testados
+    @max_k - Número de clusters
+'''
+def plot_elbow_method(features: pd.DataFrame, max_k: int):
+    inertia = []
+    K = range(2, max_k + 1)
+    for k in K:
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init='auto')
+        kmeans.fit(features)
+        inertia.append(kmeans.inertia_)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(K, inertia, 'bo-', label='Inertia')
+    plt.xlabel('Number of clusters (k)')
+    plt.ylabel('Inertia')
+    plt.title('Elbow Method for Optimal k')
+    plt.legend()
+
+    # Salvar o gráfico em um arquivo
+    plt.savefig("kmeans/graphs/elbow_method.png", dpi=300, bbox_inches='tight')  # DPI ajusta a resolução
+    plt.close()  # Fecha a figura para evitar sobreposição em gráficos futuros
+
+
+
+''' 
+    Função para plotar o gáfico da pontuação de silhueta
+    @param features - DataFrame com os dados a serem testados
+    @max_k - Número de clusters
+'''
+def plot_silhouette_scores(features: pd.DataFrame, max_k: int):
+    silhouette_scores = []
+    K = range(2, max_k + 1)
+    for k in K:
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init='auto')
+        labels = kmeans.fit_predict(features)
+        silhouette_avg = silhouette_score(features, labels, metric='euclidean')
+        silhouette_scores.append(silhouette_avg)
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(K, silhouette_scores, 'ro-', label='Silhouette Score')
+    plt.xlabel('Number of clusters (k)')
+    plt.ylabel('Silhouette Score')
+    plt.title('Silhouette Score for Optimal k')
+    plt.legend()
+
+    # Salvar o gráfico em um arquivo
+    plt.savefig("kmeans/graphs/silhouette_score.png", dpi=300, bbox_inches='tight')  # DPI ajusta a resolução
+    plt.close()  # Fecha a figura para evitar sobreposição em gráficos futuros
+
+
+
+''' 
+    Função para plotar o gráfico dos clusters PCA
+    @param features - DataFrame com os dados a serem testados
+    @max_k - Número de clusters
+'''
+def plot_pca_clusters(features: pd.DataFrame, labels: np.ndarray):
+    from sklearn.decomposition import PCA
+    pca = PCA(n_components=2)
+    pca_features = pca.fit_transform(features)
+    plt.figure(figsize=(10, 6))
+    plt.scatter(pca_features[:, 0], pca_features[:, 1], c=labels, cmap='viridis', marker='.')
+    plt.xlabel('Principal Component 1')
+    plt.ylabel('Principal Component 2')
+    plt.title('PCA of Clustered Data')
+    plt.colorbar()
+
+    # Salvar o gráfico em um arquivo
+    plt.savefig("kmeans/graphs/pca_clusters.png", dpi=300, bbox_inches='tight')  # DPI ajusta a resolução
+    plt.close()  # Fecha a figura para evitar sobreposição em gráficos futuros
+
+
+
+'''
+    Função para plotar o gráfico dos clusters com variáveis originais
+    @param features - DataFrame com os dados a serem testados
+    @param labels - Array com os rótulos de cluster gerados pelo KMeans
+'''
+def plot_original_clusters(features: pd.DataFrame, labels: np.ndarray):
+    plt.figure(figsize=(10, 6))
+    plt.scatter(features['duration'], features['comment_count'], c=labels, cmap='viridis', marker='.')
+    plt.xlabel('Duration (seconds)')
+    plt.ylabel('Comment count')
+    plt.title('Clusters Based on Original Data')
+    plt.colorbar(label='Cluster')
+    
+    # Salvar o gráfico em um arquivo
+    plt.savefig("kmeans/graphs/original_clusters.png", dpi=300, bbox_inches='tight')  # DPI ajusta a resolução
+    plt.close()  # Fecha a figura para evitar sobreposição em gráficos futuros
+
+
+
+
+''' 
+    Função para calcular Silhouette Score e Inertia do modelo treinado na base de dados
+    @param features - DataFrame com os dados a serem testados
+    @param n - Número de clusters a serem testados
+    @return results - Lista com as
+'''
+def caluculate_metrics(features: pd.DataFrame, n: int) -> List[Dict[str, float]]:
+    # Testar se a variável 'n' é inválida para esse contexto
+    if n < 3: n = 3
+
+    # Definir o intervalo de clusters a serem testados
+    K = range(2, n)
+
+    # Definir dicionário que armazenará as pontuações dos modelos
+    results = []
+
+    # Iniciar a repetição
+    for k in K:
+        # Criar a instância do KMeans
+        kmeans = KMeans(n_clusters=k, random_state=0, n_init='auto')
+
+        # Treinar o modelo e gerar os rótulos
+        labels = kmeans.fit_predict(preprocessing.normalize(features))  # labels será um numpy.ndarray
+
+        # Testar o silhouette_score e a inertia do modelo treinado
+        silhouette_avg = silhouette_score(features, labels, metric='euclidean')
+        inertia = kmeans.inertia_
+
+        # Adicionar os resultados encontrado nas listas
+        results.append({
+            'k': k,
+            'silhouette_score': silhouette_avg,
+            'inertia': inertia
+        })
+
+    # Retornar
+    return results
+
+
+
+
+''''
+    Função para selecionar o melhor número de clusters considerando ambos os critérios'
+    @param result - Lista com a pontuação de cada cluster
+    @return k = Número de clusters com a melhor pontuação
+'''
+def find_best_k(results: List[Dict[str, float]]) -> int:
+    # Definir dados locais
+    best_k = None
+    melhor_pontuacao = float('-inf')
+    
+    for resultado in results:
+        # Identificar valores do dicionário
+        k = resultado['k']
+        silhouette = resultado['silhouette_score']
+        inertia = resultado['inertia']
+
+        # Considerando a Silhouette Score como critério principal e penalizando levemente a Inertia
+        pontuacao = silhouette - (inertia * 0.0001)
+
+        # Testar a pontuação        
+        if pontuacao > melhor_pontuacao:
+            melhor_pontuacao = pontuacao
+            best_k = k
+    
+    # Retornar
+    return best_k
+
+
+
+
+# Função para tratar e normalizar os dados do DataFrame
+def normalize_data(df: pd.DataFrame, columns: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # Remover as linhas que possuem valores inválidos
+    df = df.dropna(subset=columns)
+
+    # Selecionar colunas relevantes para a clusterização
+    df_features = df[columns]
+
+    # Aplicar a padronização dos dados com média 0 e desvio padrão 1
+    scaler = MinMaxScaler()
+    df_normalized_features = scaler.fit_transform(df_features)
+
+    # Retornar o DataFrame original sem as linhas inválidas e o DataFrame normalizado
+    return df, df_normalized_features
+
+# Função para reduzir os dados do DataFrame, tirando os 10% com mais tempo e os 10% com menos tempo
+def reduce_data(df: pd.DataFrame, columns: List[str]) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    # Selecionar colunas relevantes para a redução
+    df_features = df[columns]
+
+    # Calcular os limites dos percentis
+    # lower_limit = df_features['duration'].quantile(0.10)  # 10% menor duração
+    upper_limit = df_features['duration'].quantile(0.90)  # 10% maior duração
+
+    # Filtrar os dados que estão entre os percentis
+    # df_reduced_features = df_features[(df_features['duration'] > lower_limit) & (df_features['duration'] < upper_limit)]
+    df_reduced_features = df_features[(df_features['duration'] < upper_limit)]
+
+    # Retornar o DataFrame reduzido
+    return df_reduced_features
+
+# Função controladora do algoritmo KMeans
+def kmeans(n: int, columns: List[str], normalized: bool, reduced: bool, fixed: bool, plot: bool, show_videos: bool):
+    # Testar quantidade de tentativas
+    if n < 3: n = 3
+
+    # Importar base de dados
+    df = pd.read_csv('kmeans/kmeans_video.csv')
+        
+    # Testar se é para reduzir os dados
+    if reduced:
+        df = reduce_data(df, columns)
+
+    # Testar se é para normalizar os dados
+    if normalized:
+        df, df_normalized = normalize_data(df, columns)
+    else:
+        df_normalized = df[columns]
+
+    # Testar se é para encontrar o número ideal de clusters
+    if fixed:
+        best_n = int(input("Digite a quantidade fixa de clusters: "))
+    else:
+        best_n = find_best_k( caluculate_metrics(df_normalized, n) )
+
+    # Aplicar o KMeans
+    kmeans = KMeans(n_clusters=best_n, random_state=0, n_init='auto')
+    kmeans.fit(df_normalized)
+
+    # Adicionar os rótulos de cluster ao DataFrame original
+    df['cluster'] = kmeans.labels_
+
+    # Exibir o número de clusters
+    print("Número de clusters:", best_n)
+
+    # Contar a quantidade de ocorrências de cada cluster
+    cluster_counts = df['cluster'].value_counts()
+
+    # Exibir a contagem de ocorrências de cada cluster
+    print("Contagem de ocorrências por cluster:")
+    print(cluster_counts)
+
+    # Testar se é para mostrar os gráficos da quantidade de cluster
+    if plot:
+        plot_elbow_method(df_normalized, 10)
+        plot_silhouette_scores(df_normalized, 10) 
+        plot_pca_clusters(df_normalized, kmeans.labels_)
+        if len(columns) == 2:
+            plot_original_clusters(df_normalized, kmeans.labels_)
+    
+        # Obter os valores únicos da coluna 'cluster'
+        unique_clusters = df['cluster'].unique()
+
+        # Analisar as características principais de cada cluster
+        for cluster in unique_clusters:
+            print(f'\nCluster {cluster}:')
+            cluster_data = df[df['cluster'] == cluster]
+            print(cluster_data.describe().loc[['mean', 'std'], columns])
+            # Exibir alguns exemplos de comentários no cluster
+            if show_videos:
+                print("\nExemplos de vídeos:")
+                print(cluster_data[['video_id', 'duration', 'comment_count']].head(10))
+
+    # Salvar dados em arquivo separado
+    df.to_csv("kmeans/kmeans_video_clustered.csv", index=False)
+
+
+if __name__ == "__main__":
+    # Definir as colunas de interesse
+    # columns = ['duration', 'comment_count']
+    # columns = ['duration', 'comment_count', 'view_count']
+    # columns = ['duration', 'comment_count', 'like_count']
+    columns = ['duration', 'comment_count', 'like_count', 'view_count']
+
+    # Chamar a execução
+    kmeans(n=10, columns=columns, normalized=False, reduced=True, fixed=False, plot=True, show_videos=False)
---
+++ Coletor/crawler/kmeans/separate_videos.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import re
+import os
+
+'''
+    Função para converter a duração do vídeo no formato entregue pela API do Youtube para segundos
+    @param str_duration - Duração no formato antigo
+    @return duration - Duração em segundos
+'''
+def convertDuration(str_duration: str) -> int:
+    # Expressão regular para capturar minutos (M) e segundos (S)
+    match = re.match(r"PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?", str_duration)
+    
+    # Testar se a string não atende ao padrão
+    if not match:
+        return -1
+    
+    horas = int(match.group(1)) if match.group(1) else 0
+    minutos = int(match.group(2)) if match.group(2) else 0
+    segundos = int(match.group(3)) if match.group(3) else 0
+
+    return horas * 3600 + minutos * 60 + segundos
+
+'''
+    Função para atuar na pasta individual do vídeo
+    @param dir_path - Caminho para a pasta do vídeo
+'''
+def videoFolderFuncion(dir_path: str, dados: list[list[int, int]], columns: list[str]):
+    # Criar caminho para o arquivo 'videos_info.csv' dentro da pasta do vídeo
+    csv_videos_path = os.path.join(dir_path, 'videos_info.csv')
+
+    # Testar se o arquivo existe
+    if not os.path.exists(csv_videos_path):
+        print(f"Erro! [{csv_videos_path}] não existe! (5)")
+        return
+    
+    # Definir dados
+    df_video_info = pd.read_csv(csv_videos_path)
+
+    # Remover as linhas que possuem valores inválidos
+    df_video = df_video_info.dropna(subset=columns)
+
+    # Separar as colunas de interesse
+    columns_video = df_video[columns]
+
+    # Separa a linha do vídeo
+    linha = columns_video.iloc[0].tolist()
+
+    # Tratar os dados da linha do vídeo
+    linha_tratada = [convertDuration(linha[0]), int(linha[1]), int(linha[2]), int(linha[3]), df_video['video_id'].iloc[0]]
+
+    # Adicionar a linha à lista principal
+    dados.append(linha_tratada)    
+
+'''
+    Função para percorrer toda a estrutura de pastas dos arquivos coletados
+    @param func - Função para ser aplicada dentro da pasta do vídeo
+'''
+def findVideoFolder(func, columns: list[str]):
+    # Definir dados
+    base_dir = "files"
+    dados = []
+
+    # Percorrer youtubers
+    for ytb_folder in os.listdir(base_dir):
+        # Criar caminho 'files/ytb_folder'
+        next_ytb_dir = os.path.join(base_dir, ytb_folder)
+
+        # Testar se a pasta existe
+        if not os.path.isdir(next_ytb_dir):
+            # print(f"Erro! [{next_ytb_dir}] não existe! (1)")
+            continue
+
+        # Percorrer as pastas dos anos (dentro de 'files/ytb_folder')
+        for year_folder in os.listdir(next_ytb_dir):
+            # Criar caminho 'files/ytb_folder/year_folder'
+            next_year_dir = os.path.join(next_ytb_dir, year_folder)
+
+            # Testar se a pasta existe
+            if not os.path.isdir(next_year_dir):
+                # print(f"Erro! [{next_year_dir}] não existe! (2)")
+                continue
+
+            # Percorrer as pastas dos meses (dentro de 'files/ytb_folder/year_folder')
+            for month_folder in os.listdir(next_year_dir):
+                # Criar caminho 'files/ytb_folder/year_folder/month_folder'
+                next_month_dir = os.path.join(next_year_dir, month_folder)
+
+                # Testar se a pasta existe
+                if not os.path.isdir(next_month_dir):
+                    # print(f"Erro! [{next_month_dir}] não existe! (3)")
+                    continue
+
+                # Percorrer as pastas dos vídeos (dentro de 'files/ytb_folder/year_folder/month_folder/video_folder')
+                for video_folder in os.listdir(next_month_dir):
+                    # Criar caminho 'files/ytb_folder/year_folder/month_folder/video_folder'
+                    next_video_dir = os.path.join(next_month_dir, video_folder)
+
+                    # Testar se a pasta existe
+                    if not os.path.isdir(next_video_dir):
+                        # print(f"Erro! [{next_video_dir}] não existe! (4)")
+                        continue
+
+                    # Chamar função passada como parâmetro para atuar na pasta do vídeo
+                    func(next_video_dir, dados, columns)
+
+    # Converter a lista para DataFrame
+    df = pd.DataFrame(dados, columns=columns)  # Convertendo a lista para DataFrame
+
+    df2 = df.drop_duplicates(subset=["video_id"])
+
+    # Salvar a lista gerada em um arquivo .csv
+    df2.to_csv("kmeans/kmeans_video.csv", index=False)
+
+    # Mostrar mensagem de conclusão
+    print(f"Análise concluída com sucesso! ({len(dados)} linhas)")
+
+# Testing
+if __name__ == "__main__":
+    # Definir colunas de interesse
+    columns = ['duration', 'comment_count', 'view_count', 'like_count', 'video_id']
+
+    # Chamar a função para percorrer as pastas
+    findVideoFolder( videoFolderFuncion, columns )
